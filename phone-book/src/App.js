1e2fd1bf30a0accc4030a4b13148aef55f1f5cdb
LeeJuhae <tlatmsdlwngo@naver.com>
2020-08-28 14:50:04 +0900

    Add PhoneForm Component

---
+++ phone-book/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PhoneForm from './components/PhoneForm';
+
+class App extends Component {
+  handleCreate = (data) => {
+    console.log(data);
+  }
+  render () {
+    return (
+      <PhoneForm
+        onCreate={this.handleCreate}
+      />
+    );
+  }
+}
+export default App;
